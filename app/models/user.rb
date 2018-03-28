a22d78c3f395cd5f6abfa968969bc5e4ce451771
PrinceNorin <norin.hor@gmail.com>
2018-03-28 19:40:52 +0700

    disable statement delete & remove recover password

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable
-  devise :database_authenticatable, :recoverable, :rememberable, :trackable, :validatable
+  # :confirmable, :lockable, :timeoutable and :omniauthable, :registerable, :recoverable
+  devise :database_authenticatable, :rememberable, :trackable, :validatable
 end
